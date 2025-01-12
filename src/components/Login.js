6fe870b70252ae534e34a2958b43f01064a8af95
Biraj Thapa <birajthapa983@gmail.com>
2025-01-12 22:07:37 +0545

    sign in and sign up form created

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,12 @@
 import Header from "./Header";
+import { useState } from "react";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
@@ -11,15 +17,32 @@ const Login = () => {
         />
       </div>
       <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 z-10 bg-opacity-80 text-white ">
-        <h1 className="font-extrabold font text-3xl py-3 ">Sign In</h1>
+        <h1 className="font-extrabold font text-3xl py-3 ">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+       { !isSignInForm &&<input
+          type="text"
+          placeholder="Enter your name"
+          className="p-4 my-4 w-full bg-gray-600 rounded-lg"
+        />}
         <input
           type="text"
           placeholder="Email or mobile number"
           className="p-4 my-4 w-full bg-gray-600 rounded-lg"
         />
-        <input type="password" placeholder="Password" className="p-4 my-4 w-full bg-gray-600 rounded-lg" />
-        <button className="text-center p-4 my-6 bg-red-800 opacity-100 w-full rounded-lg">Sign In</button>
-        <p className="py-4">New to Netflix? Sign up now.</p>
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-600 rounded-lg"
+        />
+        <button className="text-center p-4 my-6 bg-red-800 opacity-100 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer " onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign up now."
+            : "Already a User? sign in now"}
+        </p>
       </form>
     </div>
   );
