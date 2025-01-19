02beacd1295e6d7b33ce7a400af117e483762a88
Biraj Thapa <birajthapa983@gmail.com>
2025-01-19 10:20:18 +0545

    routing browse fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,33 @@
-import { getAuth, signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
-import { removeUser } from "../utils/userSlice";
+import { addUser, removeUser } from "../utils/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Header = () => {
-   const navigate=useNavigate();
-   const user=useSelector(state=>state.user)
-   const dispatch=useDispatch()
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const handleSignOut = () => {
     signOut(auth)
-      .then(() => {
-        navigate("/")
-      })
+      .then(() => {})
       .catch((error) => {
-        navigate("error")
+        navigate("error");
       });
   };
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid, email, displayName, photoURL }));
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+  }, []);
   return (
     <div className="absolute px-7 py-3 bg-gradient-to-b from-black to-transparent top-0 w-full z-10 flex justify-between items-center">
       <img
@@ -24,20 +35,17 @@ const Header = () => {
         src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="logo"
       />
-      {user&&<div className="flex items-center space-x-3">
-        <img
-          className="w-10"
-          src={user?.photoURL}
-          alt="user-Icon"
-        ></img>
-        <button
-          onClick={handleSignOut}
-          className="text-white font-extrabold text-lg"
-        >
-          Sign Out
-        </button>
-      </div>
-}
+      {user && (
+        <div className="flex items-center space-x-3">
+          <img className="w-10" src={user?.photoURL} alt="user-Icon"></img>
+          <button
+            onClick={handleSignOut}
+            className="text-white font-extrabold text-lg"
+          >
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
